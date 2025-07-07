727dc9d5908d59e5f9d3ac1a73e8b54732fb2c2a
meo-young <meoyoung000@naver.com>
2025-07-08 02:22:29 +0900

    [Add] Define 클래스 구현

---
+++ Source/GetOutOf/Define/DefineClass.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define SURFACE_CONCRETE    SurfaceType1
+#define SURFACE_WATER       SurfaceType2
+#define SURFACE_METAL       SurfaceType3
+#define SURFACE_DIRT        SurfaceType4
