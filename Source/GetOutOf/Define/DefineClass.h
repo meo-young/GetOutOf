5c19e2286fa7dd5841f229aa90a5e53bb5a93366
meo-young <meoyoung000@naver.com>
2025-07-08 13:19:26 +0900

    [Update] SFX Enum 추가

--- Source/GetOutOf/Define/DefineClass.h
+++ Source/GetOutOf/Define/DefineClass.h
@@ -1,6 +1,15 @@
 #pragma once
 
-#define SURFACE_CONCRETE    SurfaceType1
-#define SURFACE_WATER       SurfaceType2
-#define SURFACE_METAL       SurfaceType3
-#define SURFACE_DIRT        SurfaceType4
+#define SURFACE_CONCRETE	SurfaceType1
+#define SURFACE_WATER		SurfaceType2
+#define SURFACE_METAL		SurfaceType3
+#define SURFACE_DIRT		SurfaceType4
+
+UENUM(BlueprintType)
+enum class ESFX : uint8
+{
+	WaterFootStep = 0,
+	DirtFootStep  = 1,
+	ConcreteFootStep = 2,
+	MetalFootStep = 3,
+};
