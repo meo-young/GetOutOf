4591ad3095f0aea4e6e3d8f0118f61038f10f491
meo-young <meoyoung000@naver.com>
2025-07-13 23:10:58 +0900

    [Update] LevelSequenceDataTable 활용
    - 시작 시 현재 스테이지에 맞는 LevelSequence 출력

--- Source/GetOutOf/SubSystem/StageSubSystem.cpp
+++ Source/GetOutOf/SubSystem/StageSubSystem.cpp
@@ -1,5 +1,30 @@
 #include "SubSystem/StageSubSystem.h"
 #include "GetOutOf.h"
+#include "Character/Component/PostProcessEffectComponent.h"
+#include "GameFramework/Character.h"
+
+UStageSubSystem::UStageSubSystem()
+{
+	ConstructorHelpers::FObjectFinder<UDataTable> DT_LevelSequenceDataTable(TEXT("/Game/_GetOutOf/DataTable/DT_LevelSequence"));
+	if (DT_LevelSequenceDataTable.Succeeded())
+	{
+		LevelSequenceDataTable = DT_LevelSequenceDataTable.Object;
+	}
+}
+
+void UStageSubSystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	if (IsValid(LevelSequenceDataTable))
+	{
+		LevelSequenceDataTable->GetAllRows<FLevelSequenceDataTable>(TEXT(""), LevelSequenceRows);
+	}
+	else
+	{
+		LOG(Error, TEXT("LevelSequenceDataTable이 유효하지 않습니다"));
+	}
+}
 
 void UStageSubSystem::StartStage()
 {
@@ -8,6 +33,15 @@ void UStageSubSystem::StartStage()
 		LOG(Log, TEXT("스테이지 시작"));
 		OnStageStartedDelegate.Broadcast();
 	}
+
+	if (LevelSequenceRows.IsValidIndex(CurrentStageNum))
+	{
+		CurrentStartLSA = GetWorld()->SpawnActor<AGOOLevelSequenceActor>(LevelSequenceRows[CurrentStageNum]->StartLevelSequenceActor);
+		CurrentStartLSA->PlayLevelSequenceForLoop();
+	}
+
+	UPostProcessEffectComponent* ProcessEffectComponent = GetWorld()->GetFirstPlayerController()->GetCharacter()->FindComponentByClass<UPostProcessEffectComponent>();
+	ProcessEffectComponent->OnCameraFlashEndedDelegate.AddUObject(this, &UStageSubSystem::ShowEndLevelSequence);
 }
 
 void UStageSubSystem::EndStage()
@@ -16,5 +50,26 @@ void UStageSubSystem::EndStage()
 	{
 		LOG(Log, TEXT("스테이지 종료"));
 		OnStageEndedDelegate.Broadcast();
+	}
+
+	if (IsValid(CurrentStartLSA))
+	{
+		CurrentEndLSA->StopLevelSequence();
+	}
+
+	++CurrentStageNum;
+}
+
+void UStageSubSystem::ShowEndLevelSequence()
+{
+	if (IsValid(CurrentStartLSA))
+	{
+		CurrentStartLSA->StopLevelSequence();
+	}
+	
+	if (LevelSequenceRows.IsValidIndex(CurrentStageNum))
+	{
+		CurrentEndLSA = GetWorld()->SpawnActor<AGOOLevelSequenceActor>(LevelSequenceRows[CurrentStageNum]->EndLevelSequenceActor);
+		CurrentEndLSA->PlayLevelSequence();
 	}
 }
--- Source/GetOutOf/SubSystem/StageSubSystem.h
+++ Source/GetOutOf/SubSystem/StageSubSystem.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "DataTable/LevelSequenceDataTable.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "StageSubSystem.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnStageStarted);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnStageEnded);
 
-UCLASS()
+UCLASS(Blueprintable)
 class GETOUTOF_API UStageSubSystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
+
+public:
+	UStageSubSystem();
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 
 public:
 	/** 스테이지 시작 시 호출되는 델리게이트 */
@@ -27,5 +32,29 @@ public:
 
 	/** 스테이지 종료 함수 */
 	void EndStage();
+
+private:
+	/** 현재 스테이지의 EndLevelSequence를 호출하는 함수 */
+	void ShowEndLevelSequence();
+
+protected:
+	/** 스테이지별 LevelSequence 데이터 테이블 */
+	UPROPERTY(EditDefaultsOnly, Category = "DataTable")
+	UDataTable* LevelSequenceDataTable;
+
+	/** 스테이지별 LevelSequence 정보 */
+	TArray<FLevelSequenceDataTable*> LevelSequenceRows;
+
+private:
+	/** 현재 스테이지 번호를 나타내는 변수 */
+	uint8 CurrentStageNum = 0;
+
+	/** 현재 StartLevelSequence에 대한 캐싱 데이터 */
+	UPROPERTY()
+	AGOOLevelSequenceActor* CurrentStartLSA;
+
+	/** 현재 EndLevelSequence에 대한 캐싱 데이터 */
+	UPROPERTY()
+	AGOOLevelSequenceActor* CurrentEndLSA;
 	
 };
